8cdab52ed5c42142dd3e0e8bc1b1f1656ea4fd7e
Sakan <sakanbeer@gmail.com>
2025-10-12 23:32:39 +0700

    fix: remove .js extension from socket import for compatibility

--- src/socket.ts
+++ src/socket.ts
@@ -2,4 +2,4 @@
 
 // Legacy export - components should use useSocket hook instead
 // This file is kept for backward compatibility
-export { useSocket } from "./contexts/SocketContext.js";
+export { useSocket } from "./contexts/SocketContext";
